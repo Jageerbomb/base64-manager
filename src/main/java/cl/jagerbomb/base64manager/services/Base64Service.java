735161ad2615942624c75ff680c6294153773e82
Rodrigo Guerrero <roguerrero.go@gmail.com>
2022-05-31 00:44:03 -0400

    refactor: se ordeno codigo

--- src/main/java/cl/jagerbomb/base64manager/services/Base64Service.java
+++ src/main/java/cl/jagerbomb/base64manager/services/Base64Service.java
@@ -2,5 +2,6 @@ package cl.jagerbomb.base64manager.services;
 
 public interface Base64Service {
     String encodeStringToBase64(String url);
+
     String decodeBase64ToString(String base64);
 }
--- src/test/java/cl/jagerbomb/base64manager/Base64ManagerApplicationTests.java
+++ src/test/java/cl/jagerbomb/base64manager/Base64ManagerApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-
 class Base64ManagerApplicationTests {
 
     @Autowired
